a31a7c97074cfd51b572bbbd0d7416c8890ccb4f
EliteYarik <noi429074@yandex.ru>
2023-05-26 15:59:40 +0400

    v1.0

---
+++ src/CustomDiv.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CustomDiv.css';
+
+function CustomDiv() {
+    return (
+      <div className="custom">
+  
+      </div>
+    );
+  }
+
+  export default CustomDiv;
